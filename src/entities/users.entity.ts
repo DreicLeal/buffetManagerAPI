b47d51c7a4ff0344138541283bb6346c658c97f2
unknown <dreicleal@hotmail.com>
2023-09-30 10:47:11 -0300

    fixing adm field

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -1,40 +1,37 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    BeforeUpdate,
-    BeforeInsert,
-    OneToMany,
-  } from "typeorm";
-  import { getRounds, hashSync } from "bcryptjs";
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeUpdate,
+  BeforeInsert,
+  OneToMany,
+} from "typeorm";
+import { getRounds, hashSync } from "bcryptjs";
 import { User_dish } from "./users_dishes.entity";
 
-  @Entity("users")
-  export class User {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
-  
-    @Column()
-    name: string;
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column()
-    password: string;
+  @Column()
+  name: string;
 
-  
-    @Column({nullable: true})
-    is_adm: boolean;
-  
-  
-    @BeforeUpdate()
-    @BeforeInsert()
-    async hashPassword() {
-      const isEncrypted = getRounds(this.password);
-      if (!isEncrypted) {
-        this.password = hashSync(this.password, 10);
-      }
+  @Column()
+  password: string;
+
+  @Column({ nullable: true, default: false })
+  is_adm: boolean;
+
+  @BeforeUpdate()
+  @BeforeInsert()
+  async hashPassword() {
+    const isEncrypted = getRounds(this.password);
+    if (!isEncrypted) {
+      this.password = hashSync(this.password, 10);
     }
-  
-    @OneToMany(() => User_dish, (dishes) => dishes.user)
-    dishes: User_dish[];
   }
-  
+
+  @OneToMany(() => User_dish, (dishes) => dishes.user)
+  dishes: User_dish[];
+}
