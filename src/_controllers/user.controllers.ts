5665cdc19bd05cf12e8aff5bcbd8816351508eac
unknown <dreicleal@hotmail.com>
2023-09-30 17:54:59 -0300

    get user controller

--- src/_controllers/user.controllers.ts
+++ src/_controllers/user.controllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { TUserLogin } from "../interfaces/user.interface";
-import { createUserService } from "../_services/user.services";
+import { createUserService, getUserService } from "../_services/user.services";
 
 export const createUserController = async (
   req: Request,
@@ -9,4 +9,14 @@ export const createUserController = async (
   const userInfo: TUserLogin = req["body"];
   const newUSer = await createUserService(userInfo);
   return res.status(201).json(newUSer);
+};
+
+export const getUserController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const userId: string = req["user"];
+
+  const returnedUSer = await getUserService(userId);
+  return res.status(200).json(returnedUSer);
 };
