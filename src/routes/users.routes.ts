ce4f3a7df578316a4ec75a5f0d02b7fc04fc2842
unknown <dreicleal@hotmail.com>
2023-09-30 10:45:36 -0300

    adding create user route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,3 +1,7 @@
 import { Router } from "express";
+import { createUserController } from "../_controllers/user.controllers";
+import { signUpFieldsMiddleware } from "../middlewares/signUpFields.middleware";
 
 export const userRouter: Router = Router();
+
+userRouter.post("", signUpFieldsMiddleware, createUserController);
