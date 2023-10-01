06e2c405bf016fad38c8363c417dfe4406c6fef3
unknown <dreicleal@hotmail.com>
2023-10-01 17:48:09 -0300

    indentation

--- src/entities/users_dishes.entity.ts
+++ src/entities/users_dishes.entity.ts
@@ -1,31 +1,25 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    BeforeInsert,
-    ManyToOne,
-  } from "typeorm";
-  import { User } from "./users.entity";
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  ManyToOne,
+} from "typeorm";
+import { User } from "./users.entity";
 import { Dish } from "./dishes.entity";
-  
-  @Entity("user_dishes")
-  export class User_dish {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
-  
-    @Column()
-    plan_type: string;
-  
-    @CreateDateColumn()
-    created_at: Date;
 
-    @ManyToOne(() => User, (user) => user.dishes, { onDelete: "CASCADE" })
-    user: User;
-  
-    @ManyToOne(() => Dish, (dishes) => dishes.users, {
-      onDelete: "CASCADE",
-    })
-    dishes: Dish;
-  }
-  
+@Entity("user_dishes")
+export class User_dish {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @ManyToOne(() => User, (user) => user.dishes, { onDelete: "CASCADE" })
+  user: User;
+
+  @ManyToOne(() => Dish, (dishes) => dishes.users, {
+    onDelete: "CASCADE",
+  })
+  dishes: Dish;
+}
