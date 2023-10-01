d3971ab2f85b250795178bcc978b0c8578a6a826
unknown <dreicleal@hotmail.com>
2023-10-01 18:09:33 -0300

    get dishes route

--- src/routes/dishes.routes.ts
+++ src/routes/dishes.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { ensureAuthMiddleware } from "../middlewares/ensureAuth.middleware";
-import { createDishController, getDishController } from "../_controllers/dish.controllers";
+import {
+  createDishController,
+  getDishController,
+  getDishesController,
+} from "../_controllers/dish.controllers";
 
 export const dishRouter: Router = Router();
 
 dishRouter.post("", ensureAuthMiddleware, createDishController);
 dishRouter.get("/:id", getDishController);
+dishRouter.get("", getDishesController);
 dishRouter.patch("");
 dishRouter.delete("");
