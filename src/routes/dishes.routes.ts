35302ab317bacb105ad1b67944e40535e1b3c3b7
unknown <dreicleal@hotmail.com>
2023-10-01 17:45:38 -0300

    including dish routes

--- src/routes/dishes.routes.ts
+++ src/routes/dishes.routes.ts
@@ -1,3 +1,10 @@
 import { Router } from "express";
+import { ensureAuthMiddleware } from "../middlewares/ensureAuth.middleware";
+import { createDishController } from "../_controllers/dish.controllers";
 
 export const dishRouter: Router = Router();
+
+dishRouter.post("", ensureAuthMiddleware, createDishController);
+dishRouter.get("");
+dishRouter.patch("");
+dishRouter.delete("");
