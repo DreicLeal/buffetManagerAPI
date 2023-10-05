3cfbe00223eb2f5c53a2da1a1938fb27d75eaaa9
unknown <dreicleal@hotmail.com>
2023-10-05 13:27:02 -0300

    get messages service

--- src/_services/messages.service.ts
+++ src/_services/messages.service.ts
@@ -7,16 +7,39 @@ import { messagePostSchema } from "../schemas/message.schema";
 export const postMessageService = async (userId: string, content) => {
   const userRepository = dataSource.getRepository(User);
   const messagesRepository = dataSource.getRepository(Message);
-  const foundUser = await  userRepository.findOneBy({ id: userId });
+  const foundUser = await userRepository.findOneBy({ id: userId });
 
   if (!foundUser) {
     throw new AppError("User not found", 404);
   }
 
-  const newMessage = messagesRepository.create({...content, user:foundUser.id} );
+  const newMessage = messagesRepository.create({
+    ...content,
+    user: foundUser.id,
+  });
   await messagesRepository.save(newMessage);
 
   const messageResponse = messagePostSchema.parse(newMessage);
 
   return messageResponse;
+};
+
+export const getMessagesService = async () => {
+  const messagesRepository = dataSource.getRepository(Message);
+  const allMessages = await messagesRepository.find({
+    relations: {
+      user: true,
+    },
+    select: {
+      user: {
+        id: true,
+        name: true,
+      },
+    },
+  });
+  if (allMessages.length === 0) {
+    throw new AppError("No messages registered.", 404);
+  }
+
+  return allMessages;
 };
