f83a45c409714eb1f3aea9817deb025aa194642d
unknown <dreicleal@hotmail.com>
2023-10-05 13:26:26 -0300

    get messages controller

--- src/_controllers/messages.controller.ts
+++ src/_controllers/messages.controller.ts
@@ -1,10 +1,23 @@
-import { Request, Response } from "express"
-import { postMessageService } from "../_services/messages.service"
+import { Request, Response } from "express";
+import {
+  getMessagesService,
+  postMessageService,
+} from "../_services/messages.service";
 
-export const postMessageController = async (req:Request, res:Response):Promise<Response> => {
-    const content = req["body"]
-    const userId = req["user"].id
-    const newMessage = await postMessageService(userId, content)
-    return res.status(201).json(newMessage)
+export const postMessageController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const content = req["body"];
+  const userId = req["user"].id;
+  const newMessage = await postMessageService(userId, content);
+  return res.status(201).json(newMessage);
+};
 
-}
+export const getMessagesController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const messages = await getMessagesService();
+  return res.status(200).json(messages);
+};
