41650525fdf7b5707aade5deb076c06f20ddedbd
unknown <dreicleal@hotmail.com>
2023-10-05 13:55:40 -0300

    update message route

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { ensureAuthMiddleware } from "../middlewares/ensureAuth.middleware";
-import { getMessagesController, postMessageController } from "../_controllers/messages.controller";
+import { getMessagesController, postMessageController, updateMessageController } from "../_controllers/messages.controller";
 
 export const messageRouter: Router = Router()
 
 messageRouter.post("", ensureAuthMiddleware, postMessageController)
 messageRouter.get("", ensureAuthMiddleware, getMessagesController )
+messageRouter.patch("/:id", ensureAuthMiddleware, updateMessageController )
