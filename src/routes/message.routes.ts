6b4f2e459faa26200639cb5c9f6d1d4f9b998bbb
unknown <dreicleal@hotmail.com>
2023-10-05 13:28:36 -0300

    get messages route

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { ensureAuthMiddleware } from "../middlewares/ensureAuth.middleware";
-import { postMessageController } from "../_controllers/messages.controller";
+import { getMessagesController, postMessageController } from "../_controllers/messages.controller";
 
 export const messageRouter: Router = Router()
 
 messageRouter.post("", ensureAuthMiddleware, postMessageController)
-// messageRouter.get("", ensureAuthMiddleware, getMessagesController )
+messageRouter.get("", ensureAuthMiddleware, getMessagesController )
