0c80c2a3d0b956dcb7ec445f3690d3eca4a6fde9
unknown <dreicleal@hotmail.com>
2023-10-29 14:40:37 -0300

    fix: remove logs

--- src/_controllers/session.controller.ts
+++ src/_controllers/session.controller.ts
@@ -5,7 +5,6 @@ import { loginService } from "../_services/session.service";
 export const loginController = async (req: Request, res: Response) => {
   const loginInfo: TUserLogin = req.body;
   const token = await loginService(loginInfo);
-  console.log(token)
 
   return res.status(200).json({ token });
 };
